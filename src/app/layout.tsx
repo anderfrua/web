8a57778e13bace2c499e4bbfc133498101b0dbc0
Ander Fernandez <ander.fernandezrua@ikerlan.es>
2025-05-27 15:53:26 +0200

    apostrofe fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
-import PathfindingGrid from '@/components/PathfindingGrid';
+//import PathfindingGrid from '@/components/PathfindingGrid';
 import ParticleBackground from '@/components/ParticleBackground';
 
 
