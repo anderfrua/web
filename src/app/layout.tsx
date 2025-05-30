32563875334f11901e647c534911da82462a68d8
Ander Fernandez <anderrua@gmail.com>
2025-05-30 14:01:55 +0200

    intgridinit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
+import InteractiveGridBackground from '@/components/InteractiveGridBackground';
 //import PathfindingGrid from '@/components/PathfindingGrid';
 //import ParticleBackground from '@/components/ParticleBackground';
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <InteractiveGridBackground />
         <main className="relative z-10 pt-20">{children}</main>
       </body>
     </html>
